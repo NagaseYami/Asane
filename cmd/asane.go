6b835e08f9974cdd462351bc5545df2cb35504de
NagaseYami <nagaseyami@gmail.com>
2020-11-23 21:59:52 +0900

    添加LOG等级

--- cmd/asane.go
+++ cmd/asane.go
@@ -9,7 +9,24 @@ import (
 )
 
 func main() {
-	log.Infoln("幾重にも辛酸を舐め、七難八苦を超え、艱難辛苦の果て、満願成就に至る。")
+	log.Print("幾重にも辛酸を舐め、七難八苦を超え、艱難辛苦の果て、満願成就に至る。")
+
+	switch os.Getenv("LOG_LEVEL") {
+	case "Panic":
+		log.SetLevel(log.PanicLevel)
+	case "Fatal":
+		log.SetLevel(log.FatalLevel)
+	case "Error":
+		log.SetLevel(log.ErrorLevel)
+	case "Warn":
+		log.SetLevel(log.WarnLevel)
+	case "Info":
+		log.SetLevel(log.InfoLevel)
+	case "Debug":
+		log.SetLevel(log.DebugLevel)
+	case "Trace":
+		log.SetLevel(log.TraceLevel)
+	}
 
 	addr := os.Getenv("WEBSOCKET_SERVER_ADDR")
 	if addr == "" {
